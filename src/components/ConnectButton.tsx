d8fefb3b31475a4313708ab15a389f76f6ee8541
0xonerb <0xonerb@proton.me>
2024-09-26 10:46:01 -0300

    feat: add compactHash function

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,3 +1,20 @@
+'use client'
+
+import { useAccount } from 'wagmi'
+
+const compactHash = (hash: string) => {
+  return hash.slice(0, 7) + '...' + hash.slice(-5)
+}
+
 export const ConnectButton = () => {
-  return <w3m-button />
+  const account = useAccount()
+
+  const compactAddress = compactHash(account.address || '')
+
+  return (
+    <div>
+      <span className="text-black">{compactAddress}</span>
+      <w3m-button />
+    </div>
+  )
 }
