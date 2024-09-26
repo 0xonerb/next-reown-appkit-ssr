9ea915263be6cc1936280cc1ff65e928a074870d
0xonerb <0xonerb@proton.me>
2024-09-26 11:05:04 -0300

    fix: add newWagmiConfig

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,11 @@
-import { cookieStorage, createStorage } from '@wagmi/core'
+import { cookieStorage, createStorage, http } from '@wagmi/core'
+import { createConfig } from 'wagmi'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
+import {
+  arbitrum as arbitrumChain,
+  mainnet as mainnetChain
+} from 'wagmi/chains'
 
 // Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
@@ -9,7 +14,21 @@ if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
+const chains = [arbitrumChain, mainnetChain] as const
+
 export const networks = [mainnet, arbitrum]
+
+export const newWagmiConfig = createConfig({
+  chains,
+  transports: {
+    [mainnetChain.id]: http(),
+    [arbitrumChain.id]: http()
+  },
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  })
+})
 
 // Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
@@ -20,5 +39,10 @@ export const wagmiAdapter = new WagmiAdapter({
   projectId,
   networks
 })
+
+// NOTE: this `newWagmiConfig` replaces the `wagmiConfig` from AppKit
+// wagmiAdapter.wagmiConfig = newWagmiConfig
+
+console.log('wagmiAdapter.wagmiConfig', wagmiAdapter.wagmiConfig)
 
 export const config = wagmiAdapter.wagmiConfig
